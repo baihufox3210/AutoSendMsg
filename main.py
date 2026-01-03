e9710613f2ccc2b75328ae4c3a8d4826b1398dfa
baihu <baihu3210@gmail.com>
2026-01-03 10:17:59 +0800

    feat: Add SQLite database for task management and expose CRUD operations via new API endpoints.

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+import json
 import asyncio
+
 from aiohttp import web
 from datetime import datetime
 
+from src.database import database
+
 async def task(target):
-    await asyncio.sleep(1)
-    print(target)
+    await asyncio.to_thread(database.add_task, target)
 
-async def add_task(target):
-    asyncio.create_task(task(target.json()))
-    return web.json_response({"status": "ok"})
+async def add_task(request):
+    try:
+        data = await request.json()
+        await task(data)
+        return web.json_response({"status": "200"})
 
-async def get_task():
-    return web.json_response({"status": "ok"})
+    except Exception as e:
+        return web.json_response({"status": "500", "error": str(e)})
+
+async def get_task(request):
+    return web.json_response(
+        {
+            "status": "200",
+            "data": json.loads(database.get_task())
+        }
+    )
+
+async def delete_task(request):
+    try:
+        id = request.query.get("id")
+        await asyncio.to_thread(database.delete_task, id)
+        return web.json_response({"status": "200"})
+
+    except Exception as e:
+        return web.json_response({"status": "500", "error": str(e)})
 
 async def main():
     app = web.Application()
     app.add_routes([
         web.post("/add_task", add_task),
-        web.get("/get_task", get_task)
+        web.get("/get_task", get_task),
+        web.get("/delete_task", delete_task)
     ])
 
     runner = web.AppRunner(app)
--- src/database/database.py
+++ src/database/database.py
@@ -1,27 +1,32 @@
 import sqlite3
 import json
 
-connection = sqlite3.connect("src\database\database.db")
-
 def init_db():
+    connection = sqlite3.connect(r"src\database\database.db")
+
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS tasks ( id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL )")
     connection.commit()
+    connection.close()
 
 
 def add_task(task: dict):
+    connection = sqlite3.connect(r"src\database\database.db")
     cursor = connection.cursor()
     cursor.execute(
         "INSERT INTO tasks (task) VALUES (?)",
         (json.dumps(task),)
     )
     connection.commit()
+    connection.close()
 
 
 def get_task():
+    connection = sqlite3.connect(r"src\database\database.db")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM tasks")
     rows = cursor.fetchall()
+    connection.close()
 
     return json.dumps(
         [{"id": row[0], "task": json.loads(row[1])} for row in rows],
@@ -30,8 +35,10 @@ def get_task():
 
 
 def delete_task(task_id: int):
+    connection = sqlite3.connect(r"src\database\database.db")
     cursor = connection.cursor()
     cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
     connection.commit()
+    connection.close()
 
 init_db()
